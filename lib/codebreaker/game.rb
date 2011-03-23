fbce05e36020005f29936d9ab299c8220c6a6907
Rafael George <george.rafael@gmail.com>
2011-03-23 13:33:58 +0000

    move marker class to it's own file

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,33 +1,6 @@
 module Codebreaker
   class Game
 
-    class Marker
-      def initialize(secret, guess)
-        @secret, @guess = secret, guess
-      end
-
-      def number_match_count
-        (0..3).inject(0) do |count,index|
-          count +(number_match?(index) ? 1 : 0)
-        end
-      end
-
-      def exact_match_count
-        (0..3).inject(0) do |count, index|
-          count +(exact_match?(index) ? 1 : 0)
-        end
-      end
-
-      def exact_match?(index)
-        @guess[index] == @secret[index]
-      end
-
-      def number_match?(index)
-        @secret.include?(@guess[index]) && !exact_match?(index)
-      end
-
-    end
-
     def initialize(output)
       @output = output
     end
